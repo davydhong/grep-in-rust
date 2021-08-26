99aa9a1c1b7ed9f1a35aa5ba33775ddb64752d70
David Y Hong <davydhong@gmail.com>
2021-08-25 22:25:59 -0700

    eprintln

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,18 @@
 use std::env;
 use std::process;
 
-use grep_in_rust::Config;
+use minigrep::Config;
 
 fn main() {
-    // --snip--
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    if let Err(e) = grep_in_rust::run(config) {
-        // --snip--
-        println!("Application error: {}", e);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
